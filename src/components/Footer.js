164acd8933a8490f9c2636886f9d941a785228d4
AnttiAitta <n1aian00@students.oamk.fi>
2022-11-22 13:04:57 +0200

    footeriin sisältöä

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,13 @@ import '../components/Footer.css'
 export default function Footer() {
   return (
     <footer className='footer'>
-            <span>verkkopalveluprojekti</span>
+      <div>
+          <h2>PANIMON NIMI</h2> 
+          <p>Panimokatu 13, <br></br>90123 Oulu</p> 
+          <p>Contact</p>
+          <p>Tel. 010 123 4567</p>
+          <p>panimonnimi@kalja.fi</p>
+      </div>
     </footer>
   )
 }
