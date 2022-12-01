f6456991daac0bc16e19458da50fd7b49e52d87e
wldakbari <n1akwa00@students.oamk.fi>
2022-12-01 19:11:48 +0200

    added logo to images

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -20,6 +20,25 @@ export default function Carousel() {
         <div class="carousel-item">
           <img src="images/image3.jpg" class="d-block w-100" alt="..."/>
         </div>
+      <div>
+        <div class="container marketing">
+
+        <div class="row"> 
+          <div class="col-lg-4">
+          <img src="images/image1.jpg" alt="..." class="rounded-circle border border-light" width="300" height="300"/>      
+          <p><a class="btn btn-secondary" href="#">Oluet &raquo;</a></p>
+          </div>
+          <div class="col-lg-4">
+          <img src="images/image2.jpg" alt="..." class="rounded-circle border border-light" width="300" height="300"/>      
+            <p><a class="btn btn-secondary" href="#">Oheistuotteet &raquo;</a></p>
+          </div>
+          <div class="col-lg-4">
+          <img src="images/image3.jpg" alt="..." class="rounded-circle border border-light" width="300" height="300"/>      
+            <p><a class="btn btn-secondary" href="#">jotain &raquo;</a></p>
+          </div>
+        </div>
+        </div>
+      </div>
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -31,23 +50,7 @@ export default function Carousel() {
       </button>
     </div>
 
-    <div class="container marketing">
 
-    <div class="row"> 
-    <div class="col-lg-4">
-    <img src="images/image1.jpg" alt="..." class="rounded-circle" width="200" height="200"/>      
-    <p><a class="btn btn-secondary" href="#">Oluet &raquo;</a></p>
-    </div>
-    <div class="col-lg-4">
-    <img src="images/image2.jpg" alt="..." class="rounded-circle" width="200" height="200"/>      
-      <p><a class="btn btn-secondary" href="#">Oheistuotteet &raquo;</a></p>
-    </div>
-    <div class="col-lg-4">
-    <img src="images/image3.jpg" alt="..." class="rounded-circle" width="200" height="200"/>      
-      <p><a class="btn btn-secondary" href="#">jotain &raquo;</a></p>
-     </div>
-    </div>
-    </div>
     </>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import '../components/Header.css'
 
 export default function Header() {
   return (
     <header>
     <div  style={{textAlign:"center"}}><h1>Programmer's Brewing Company</h1>
-    <p>npx i create-good-beer</p></div>
+    <p>npx create-good-beer</p></div>
     </header>
   )
 }
