0769219740f99a51f16b39a8c5b975f585b43992
wldakbari <n1akwa00@students.oamk.fi>
2022-12-03 16:18:53 +0200

    Some changes to carousel css

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -11,7 +11,7 @@ export default function Carousel() {
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
       <div class="carousel-inner">
-        <div class="carousel-item active">
+        <div class="carousel-item active" data-bs-interval="1000">
           <img src="images/image1.jpg" class="d-block w-100" alt="..."/>
         </div>
         <div class="carousel-item">
@@ -20,26 +20,25 @@ export default function Carousel() {
         <div class="carousel-item">
           <img src="images/image3.jpg" class="d-block w-100" alt="..."/>
         </div>
-      <div>
         <div class="container marketing">
 
         <div class="row"> 
           <div class="col-lg-4">
-          <img src="images/image1.jpg" alt="..." class="rounded-circle border border-light" width="300" height="300"/>      
+          <img src="images/image1.jpg" alt="..." class="rounded-circle border border-light" width="200" height="200"/>      
           <p><a class="btn btn-secondary" href="#">Oluet &raquo;</a></p>
           </div>
           <div class="col-lg-4">
-          <img src="images/image2.jpg" alt="..." class="rounded-circle border border-light" width="300" height="300"/>      
+          <img src="images/image2.jpg" alt="..." class="rounded-circle border border-light" width="200" height="200"/>      
             <p><a class="btn btn-secondary" href="#">Oheistuotteet &raquo;</a></p>
           </div>
           <div class="col-lg-4">
-          <img src="images/image3.jpg" alt="..." class="rounded-circle border border-light" width="300" height="300"/>      
+          <img src="images/image3.jpg" alt="..." class="rounded-circle border border-light" width="200" height="200"/>      
             <p><a class="btn btn-secondary" href="#">jotain &raquo;</a></p>
           </div>
         </div>
         </div>
       </div>
-      </div>
+  </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
@@ -48,9 +47,6 @@ export default function Carousel() {
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
       </button>
-    </div>
-
-
     </>
   )
 }
