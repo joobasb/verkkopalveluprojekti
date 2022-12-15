4c3a78c9c20df60897a38ec5822d7ac0080d0e93
Antti Aitta <n1aian00@students.oamk.fi>
2022-12-15 14:57:50 +0200

    about-sivun css

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,23 +1,32 @@
-import React from 'react'
+import "./About.css";
+import React from "react";
 
 export default function About() {
   return (
-    <div>
-    <div>About</div>
-    <div>
-      <h2>Janon sammuttaja vuodesta 2022</h2>
+    <div className="about-container">
+      <div className="about-header">
+        <h2>Janon sammuttaja vuodesta 2022</h2>
+      </div>
+      <div className="about-text">
+        <p>
+          Programmer's Brewing Company on tradenomiopisklijoiden perustama
+          pienpanimo, <br></br>joka aloitti toimintansa vuonna 2022. <br></br>
+          Tavoitteenamme on valmistaa laadukkaita oluita, erilaisiin makuihin.
+          <br></br>valikoimastamme löytyy niin perinteisiä lageroluita, kuin
+          eksoottisempia soureja.<br></br>Meille tärkeää on tuotteidemme
+          opiskelijaystävällinen hinta, laadusta kuitenkaan tinkimättä.
+        </p>
+      </div>
+      <div className="about-listheader">
+        <h3>Yrityksemme on saanut seuraavat tunnustukset</h3>
+        <div className="about-list">
+          <ul>
+            <li>Vuoden panimo 2022</li>
+            <li>Vuoden tulokas 2022</li>
+            <li>Vuoden ekoteko 2022</li>
+          </ul>
+        </div>
+      </div>
     </div>
-    <p>Programmer's Brewing Company on tradenomiopisklijoiden perustama pienpanimo, <br>
-    </br>joka aloitti toimintansa vuonna 2022. <br>
-    </br>Tavoitteenamme on valmistaa laadukkaita oluita, erilaisiin makuihin.<br>
-    </br>valikoimastamme löytyy niin perinteisiä lager oluita, kuin eksoottisempia soureja.<br>
-    </br>Meille tärkeää on tuotteidemme opiskelijaystävällinen hinta, laadusta kuitenkaan tinkimättä.</p>
-    <ul>Yrityksemme on ansainnut seuraavat tunnustukset:
-      <li>Vuoden panimo 2022</li>
-      <li>Vuoden tulokas 2022</li>
-      <li>Vuoden ekoteko 2022</li>
-    </ul>
-    </div>
-
-  )
+  );
 }
